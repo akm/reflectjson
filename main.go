b9e0fc8dac2cd7fb5ebdf547bc4de0b6303c9fb8
akm <akm2000@gmail.com>
2019-02-11 14:20:21 +0900

    :recycle: Extract dig.go

--- main.go
+++ main.go
@@ -64,52 +64,6 @@ type DataType struct {
 	Fields  []*DataField
 }
 
-func DigTypes(types []reflect.Type) []reflect.Type {
-	m := map[string]reflect.Type{}
-	for _, t := range types {
-		key := t.PkgPath() + "." + t.Name()
-		m[key] = t
-	}
-
-	for _, t := range types {
-		DigType(m, t)
-	}
-
-	r := []reflect.Type{}
-	for _, t := range m {
-		r = append(r, t)
-	}
-	return r
-}
-
-func DigType(m map[string]reflect.Type, t reflect.Type) {
-	switch t.Kind() {
-	case reflect.Struct:
-		DigStruct(m, t)
-	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
-		DigType(m, t.Elem())
-	}
-}
-
-func DigStruct(m map[string]reflect.Type, t reflect.Type) {
-	numField := t.NumField()
-	for i := 0; i < numField; i++ {
-		f := t.Field(i)
-		ft := f.Type
-		if ft.PkgPath() != "" {
-			key := ft.PkgPath() + "." + ft.Name()
-			_, ok := m[key]
-			if !ok {
-				m[key] = ft
-				switch ft.Kind() {
-				case reflect.Struct:
-					DigType(m, ft)
-				}
-			}
-		}
-	}
-}
-
 func NewDataType(t reflect.Type) *DataType {
 	r := &DataType{
 		Name:    t.Name(),
