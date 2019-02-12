32a0c7d54687637521221fe544d7dbad780c25be
akm <akm2000@gmail.com>
2019-02-12 12:18:52 +0900

    :+1: Sort results of TypeDict.Types

--- sort.go
+++ sort.go
@@ -1,7 +1,17 @@
 package typedict
 
+import (
+	"reflect"
+)
+
 func DataTypeSorter(dataTypes []*DataType) func(int, int) bool {
 	return func(i, j int) bool {
 		return (dataTypes[i].PkgPath + "." + dataTypes[i].Name) < (dataTypes[j].PkgPath + "." + dataTypes[j].Name)
+	}
+}
+
+func ReflectTypeSorter(types []reflect.Type) func(int, int) bool {
+	return func(i, j int) bool {
+		return (types[i].PkgPath() + "." + types[i].Name()) < (types[j].PkgPath() + "." + types[j].Name())
 	}
 }
